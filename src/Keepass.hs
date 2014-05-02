0ad9eaf3018fa6a3e27b97fa67976fa12c565900
Bjørnar Snoksrud <bjornar@snoksrud.no>
2014-05-02 21:49:54 +0200

    parse the header

--- src/Keepass.hs
+++ src/Keepass.hs
@@ -2,16 +2,31 @@ module Keepass where
 
 import qualified Data.ByteString as BS
 import qualified Data.Binary.Strict.Get as BG
+
+import Data.Bits
 import Data.Word
+
+import Control.Applicative
 import Control.Monad
 
 type KEntry = (String, String)
 data KMeta = KMeta deriving (Show)
 data KDBUnlocked = KDBUnlocked KMeta [KEntry] deriving (Show)
 
-data KHeader = KHeader String deriving (Show)
 type KBody = BS.ByteString
 data KDBLocked = KDBLocked KHeader KBody deriving (Show)
+
+data KHeader = KHeader {
+    getFlags :: Word32
+  , getVer :: Word32
+  , getSeedRand :: BS.ByteString
+  , getEncIv :: BS.ByteString
+  , getNGroups :: Word32
+  , getNEntries :: Word32
+  , getChecksum :: BS.ByteString
+  , getSeedKey :: BS.ByteString
+  , getSeedRotN :: Word32
+} deriving (Show)
 
 type ParseEither a = Either String a
 
@@ -33,11 +48,33 @@ kdbVerDw  = 0x00030002
 
 parseHeader :: BS.ByteString -> ParseEither KHeader
 parseHeader header = do
+    when (BS.length header < kdbHeaderSize) $
+        fail "truncated header"
+
     let (result, header') = BG.runGet readMagicNumber header
     (magic1, magic2) <- result
-    if (magic1 == kdbSig1) && (magic2 == kdbSig2v1) then
-        return $ KHeader ""
-        else fail "Not a KeePassX file"
+
+    unless (magic1 == kdbSig1 && magic2 == kdbSig2v1) $
+        fail "Not a KeePassX file"
+
+    kheader <- fst $ BG.runGet readHeader header'
+
+    unless ((getVer kheader `xor` kdbVerDw) .&. 0xffffff00 == 0) $
+        fail "wrong DB_VER_DW"
+
+    return kheader
 
 readMagicNumber :: BG.Get (Word32, Word32)
 readMagicNumber = liftM2 (,) BG.getWord32le BG.getWord32le
+
+readHeader :: BG.Get KHeader
+readHeader = KHeader <$>
+             BG.getWord32le <*> -- flags
+             BG.getWord32le <*> -- ver
+             BG.getByteString 16 <*> -- seed_rand
+             BG.getByteString 16 <*> -- enc_iv
+             BG.getWord32le <*> -- n_groups
+             BG.getWord32le <*> -- n_entries
+             BG.getByteString 32 <*> -- checksum
+             BG.getByteString 32 <*> -- seed_key
+             BG.getWord32le -- seed_rot_n
